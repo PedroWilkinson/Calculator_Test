62c26373b38e816dde961674846ba0c6966c41c3
PedroWilkinson <dev.pedromatos@gmail.com>
2023-05-31 13:42:48 -0300

    Continuando a Calculadora teste

--- teste1.py
+++ teste1.py
@@ -17,6 +17,9 @@ bt4.place(x=10, y=155)
 bt1 = Button(master, text="1", bg="lightblue",pady=14, padx=14, bd=4)
 bt1.place(x=10, y=210)
 
+bt0 = Button(master, text="0", bg="lightblue",pady=14, padx=14, bd=4)
+bt0.place(x=10, y=265)
+
 #Coluna 2:
 
 bt8 = Button(master, text="8", bg="lightblue",pady=14, padx=14, bd=4)
@@ -28,8 +31,9 @@ bt5.place(x=60, y=155)
 bt2 = Button(master, text="2", bg="lightblue",pady=14, padx=14, bd=4)
 bt2.place(x=60, y=210)
 
-bt0 = Button(master, text="0", bg="lightblue",pady=14, padx=14, bd=4)
-bt0.place(x=60, y=265)
+bt_ponto = Button(master, text=".", bg="lightblue",pady=14, padx=15, bd=4)
+bt_ponto.place(x=60, y=265)
+
 
 #Coluna 3:
 
@@ -42,22 +46,42 @@ bt6.place(x=110, y=155)
 bt3 = Button(master, text="3", bg="lightblue",pady=14, padx=14, bd=4)
 bt3.place(x=110, y=210)
 
-bt_ponto = Button(master, text=".", bg="green",pady=14, padx=15, bd=4)
-bt_ponto.place(x=110, y=265)
+bt_exp = Button(master, text="EXP", bg="silver",pady=14, padx=7, bd=4)
+bt_exp.place(x=110, y=265)
+
 
 #Coluna 4:
 
-bt_dividir = Button(master, text="/", bg="lightblue",pady=14, padx=14, bd=4)
-bt_dividir.place(x=160, y=100)
+bt_ce = Button(master, text="CE", bg="silver",pady=14, padx=10, bd=4)
+bt_ce.place(x=160, y=100)
 
-bt_multiplicar = Button(master, text="*", bg="lightblue",pady=14, padx=14, bd=4)
+bt_multiplicar = Button(master, text="*", bg="silver",pady=14, padx=14, bd=4)
 bt_multiplicar.place(x=160, y=155)
 
-bt_subtrair = Button(master, text="-", bg="lightblue",pady=14, padx=14, bd=4)
-bt_subtrair.place(x=160, y=210)
+bt_somar = Button(master, text="+", bg="silver",pady=14, padx=13, bd=4)
+bt_somar.place(x=160, y=210)
 
-bt_somar = Button(master, text="+", bg="lightblue",pady=14, padx=14, bd=4)
-bt_somar.place(x=160, y=265)
+bt_ans = Button(master, text="ANS", bg="silver",pady=14, padx=5, bd=4)
+bt_ans.place(x=160, y=265)
+
+#Coluna 5:
+
+bt_del = Button(master, text="DEL", bg="silver",pady=14, padx=7, bd=4)
+bt_del.place(x=210, y=100)
+
+bt_dividir = Button(master, text="/", bg="silver",pady=14, padx=14, bd=4)
+bt_dividir.place(x=210, y=155)
+
+bt_subtrair = Button(master, text="-", bg="silver",pady=14, padx=14, bd=4)
+bt_subtrair.place(x=210, y=210)
+
+bt_igual = Button(master, text="=", bg="green",pady=14, padx=12, bd=4)
+bt_igual.place(x=210, y=265)
+
+
+
+
+
 
 master.resizable(False, False)
 master.geometry("280x380")
